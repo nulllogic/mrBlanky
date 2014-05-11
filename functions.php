d783c9b8d98cc4f1cd99669ca04adbe56e66d1b6
vladimirlukyanov <vladimir.lukyanoff@gmail.com>
2014-05-11 06:33:26 +0400

    v0.0.1

--- functions.php
+++ functions.php
@@ -20,44 +20,6 @@ if (!isset($content_width))
     $content_width = 900;
 }
 
-if (function_exists('add_theme_support'))
-{
-    // Add Menu Support
-    add_theme_support('menus');
-
-    // Add Thumbnail Theme Support
-    add_theme_support('post-thumbnails');
-    add_image_size('large', 700, '', true); // Large Thumbnail
-    add_image_size('medium', 250, '', true); // Medium Thumbnail
-    add_image_size('small', 120, '', true); // Small Thumbnail
-    add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
-
-    // Add Support for Custom Backgrounds - Uncomment below if you're going to use
-    /*add_theme_support('custom-background', array(
-	'default-color' => 'FFF',
-	'default-image' => get_template_directory_uri() . '/img/bg.jpg'
-    ));*/
-
-    // Add Support for Custom Header - Uncomment below if you're going to use
-    /*add_theme_support('custom-header', array(
-	'default-image'			=> get_template_directory_uri() . '/img/headers/default.jpg',
-	'header-text'			=> false,
-	'default-text-color'		=> '000',
-	'width'				=> 1000,
-	'height'			=> 198,
-	'random-default'		=> false,
-	'wp-head-callback'		=> $wphead_cb,
-	'admin-head-callback'		=> $adminhead_cb,
-	'admin-preview-callback'	=> $adminpreview_cb
-    ));*/
-
-    // Enables post and comment RSS feed links to head
-    add_theme_support('automatic-feed-links');
-
-    // Localisation Support
-    load_theme_textdomain('html5blank', get_template_directory() . '/languages');
-}
-
 /*------------------------------------*\
 	Functions
 \*------------------------------------*/
@@ -449,6 +411,40 @@ function html5_shortcode_demo_2($atts, $content = null) // Demo Heading H2 short
     return '<h2>' . $content . '</h2>';
 }
 
-$cpt = new wBoost\Core\CPT('activate');
+
+// Add Menu Support
+add_theme_support('menus');
+
+// Add Thumbnail Theme Support
+add_theme_support('post-thumbnails');
+add_image_size('large', 700, '', true); // Large Thumbnail
+add_image_size('medium', 250, '', true); // Medium Thumbnail
+add_image_size('small', 120, '', true); // Small Thumbnail
+add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
+
+// Enables post and comment RSS feed links to head
+add_theme_support('automatic-feed-links');
+
+// Localisation Support
+load_theme_textdomain('template', get_template_directory() . '/i18n/languages');
+
+// Init wBoost
+$wBoost = new wBoost\Core();
+
+// Add styles
+$wBoost->addStyle('style')
+		->addStyle('style2')
+		->addStyle('styly3');
+
+
+// Add javascript
+$wBoost->addScript('style')
+       ->addScript('style2')
+       ->addScript('styly3');
+
+
+$wBoost->get('Taxonomy')
+			->register_custom_post_type('name');
+
 
 ?>
